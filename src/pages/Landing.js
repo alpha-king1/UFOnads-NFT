10feec0ca822de65425614266540aa597323ff3d
john <oduyebojohn123@example.com>
2025-11-15 12:44:32 +0100

    changefor image

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -122,7 +122,7 @@ const UfonadsLanding = () => {
             {/* Right: Image */}
             <div className="relative">
                 <img 
-                    src="u.jpg"  
+                    src="U.jpg"  
                     className="w-full aspect-square object-cover rounded-3xl border-2 border-purple-500/30"
                 />
               {/* Floating elements */}
